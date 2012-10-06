275a25b599fa4c2b1566e255b2268106ea199a80
Sönke Ludwig <sludwig@rejectedsoftware.com>
2012-10-06 23:56:39 +0200

    Added missing file.

---
+++ source/ddox/processors/eptemplates.d
@@ -0,0 +1,26 @@
+module ddox.processors.eptemplates;
+
+import ddox.api;
+import ddox.entities;
+
+void mergeEponymousTemplates(Package root)
+{
+	void processDecls(Declaration[] decls)
+	{
+		foreach( ref d; decls ){
+			if( auto templ = cast(TemplateDeclaration)d ){
+				if( templ.members.length == 1 && templ.members[0].name == templ.name ){
+					templ.members[0].templateArgs = templ.templateArgs;
+					templ.members[0].parent = templ.parent;
+					d = templ.members[0];
+				} else processDecls(templ.members);
+			} else if( auto comp = cast(CompositeTypeDeclaration)d ){
+				processDecls(comp.members);
+			}
+		}
+	}
+
+	root.visit!Module((mod){
+		processDecls(mod.members);
+	});
+}
