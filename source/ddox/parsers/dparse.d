0065d3086e6401e7ff57f1c898afe72759aa8a97
Martin Nowak <code@dawg.eu>
2014-12-13 19:31:19 +0100

    use stable depedencies and fix building with libdparse

--- source/ddox/parsers/dparse.d
+++ source/ddox/parsers/dparse.d
@@ -97,7 +97,7 @@ private struct DParser
 		root.visit!ClassDeclaration((decl){
 			if (decl.baseClass && decl.baseClass.typeDecl && !cast(ClassDeclaration)decl.baseClass.typeDecl)
 				decl.baseClass.typeDecl = null;
-			
+
 			foreach (ref i; decl.derivedInterfaces) {
 				if (i.typeDecl && !decl.baseClass && cast(ClassDeclaration)i.typeDecl) {
 					decl.baseClass = i;
@@ -183,10 +183,10 @@ private struct DParser
 			additional_attribs ~= decl.attributes;
 			return decl.declarations.map!(d => parseDecl(d, parent, additional_attribs)).join();
 		}
-		
+
 		Declaration[] ret;
 		string comment;
-		int line;
+		size_t line;
 		if (auto fd = decl.functionDeclaration) {
 			comment = fd.comment.undecorateComment();
 			line = fd.name.line;
@@ -272,7 +272,8 @@ private struct DParser
 			return null;
 		} else if (auto ad = decl.aliasDeclaration) {
 			comment = ad.comment.undecorateComment();
-			line = ad.name.line;
+			assert(ad.initializers.length);
+			line = ad.initializers[0].name.line;
 			foreach (ai; ad.initializers) {
 				auto adr = new AliasDeclaration(parent, ai.name.text.idup);
 				adr.targetType = parseType(ai.type, parent);
@@ -328,7 +329,7 @@ private struct DParser
 			addAttributes(d, decl.attributes);
 
 			d.docGroup = new DocGroup(d, comment);
-			d.line = line;
+			d.line = line.to!int;
 		}
 
 		return ret;
